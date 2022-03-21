7c4199463a0f2bdbd5a4b0b57fad03025b27dbdf
AthirahKM <101968344+AthirahKM@users.noreply.github.com>
2022-03-21 12:48:13 +0800

    Add files via upload

---
+++ Test.FE/Controllers/PageController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test.Controllers
+{
+    public class PageController : Controller
+    {
+        public ActionResult Variant()
+        {
+            return View();
+        }
+        public ActionResult Influenza()
+        {
+            return View();
+        }
+        public ActionResult Therapeutic()
+        {
+            return View();
+        }
+        public ActionResult Government()
+        {
+            return View();
+        }
+        public ActionResult NYT()
+        {
+            return View();
+        }
+        public ActionResult Vaccine()
+        {
+            return View();
+        }
+    }
+}
